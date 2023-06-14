2916697576362435e669a8a2ed3c916bb27d62dd
Selva791 <SHERlock@93>
2023-06-14 23:06:39 +0200

    Initial commit

---
+++ src/main/java/com/messanger/MessangerApplication.java
@@ -0,0 +1,37 @@
+package com.messanger;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class MessangerApplication {
+    public static void main(String[] args) {
+        System.out.println("Kafka producer");
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo_java", "Dhoni- definitely not");
+
+        // send data - asynchronous
+        producer.send(producerRecord);
+
+        // flush data - synchronous
+        producer.flush();
+        // flush and close producer
+        producer.close();
+
+    }
+}
