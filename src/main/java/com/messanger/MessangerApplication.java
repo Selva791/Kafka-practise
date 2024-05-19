1e3de758d52c89ec7c97ea3447aa602c13a07837
Selva791 <SHERlock@93>
2024-05-20 01:13:06 +0200

    Kafka setup

--- src/main/java/com/messanger/MessangerApplication.java
+++ src/main/java/com/messanger/MessangerApplication.java
@@ -1,10 +1,17 @@
 package com.messanger;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class MessangerApplication {
@@ -14,17 +21,26 @@ public class MessangerApplication {
         String bootstrapServers = "127.0.0.1:9092";
 
         // create Producer properties
-        Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        Properties pro_properties = new Properties();
+        pro_properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        pro_properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        pro_properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+       //consumer
+        pro_properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        pro_properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        pro_properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        pro_properties.put(ConsumerConfig.GROUP_ID_CONFIG, "my-consumer-group"); // Set the group id
+
+        pro_properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         // create the producer
-        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        KafkaProducer<String, String> producer = new KafkaProducer<>(pro_properties);
 
         // create a producer record
         ProducerRecord<String, String> producerRecord =
                 new ProducerRecord<>("demo_java", "Dhoni- definitely not");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(pro_properties);
+
 
         // send data - asynchronous
         producer.send(producerRecord);
@@ -33,6 +49,18 @@ public class MessangerApplication {
         producer.flush();
         // flush and close producer
         producer.close();
+        consumer.subscribe(Arrays.asList("demo_java"));
+        while(true){
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records){
+                System.out.println("Key: " + record.key() + ", Value: " + record.value());
+                System.out.println("Partition: " + record.partition() + ", Offset:" + record.offset());
+            }
+
+        }
+
 
     }
 }
